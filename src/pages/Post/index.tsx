3773db50959165d66d900644081d37a52d02f500
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-08-18 17:54:09 -0300

    desafio concluído

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,11 +1,102 @@
+import axios from "axios";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { useEffect, useState } from "react";
+import { FaArrowLeft, FaCalendarDay, FaComment, FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
+import { Action, PostContainer, PostHeaderActions, PostHeaderContainer, PostInfoContainer } from "./styles";
+
+interface PostData {
+  html_url: string;
+  title: string;
+  user: {
+    login: string;
+  }
+  created_at: string;
+  comments: number;
+  body: string;
+}
+
 export function Post() {
+  const [post, setPost] = useState<PostData>({} as PostData);
+  const [loading, setLoading] = useState(true);
+
   const { number: postNumber } = useParams();
 
+  async function fetchPostData() {
+    setLoading(true);
+    try {
+      const response = await axios.get(`https://api.github.com/repos/rafaelmanfrim/desafio03-github-blog/issues/${postNumber}`)
+      setPost(response.data)
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPostData()
+  }, [postNumber]);
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div>
-      <h1>Post {postNumber}</h1>
-    </div>
+    <PostContainer>
+      <PostHeaderContainer>
+        <PostHeaderActions>
+          <Action to='/'>
+            <FaArrowLeft /> VOLTAR
+          </Action>
+          <Action to={post.html_url} target="_blank">
+            VER NO GITHUB <FaExternalLinkAlt />
+          </Action>
+        </PostHeaderActions>
+        <strong>{post.title}</strong>
+        <PostInfoContainer>
+          <span>
+            <FaGithub /> {post.user.login}
+          </span>
+          <span>
+            <FaCalendarDay /> {formatDistanceToNow(new Date(post.created_at), {
+              locale: ptBR,
+              addSuffix: true,
+            })}
+          </span>
+          {post.comments > 0 && (
+            <span>
+              <FaComment /> {post.comments} {post.comments === 1 ? "seguidor" : "seguidores"}
+            </span>
+          )}
+        </PostInfoContainer>
+      </PostHeaderContainer>
+      <ReactMarkdown
+        children={post.body}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                style={dracula as any}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    </PostContainer>
   );
 }
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -1,1 +1,99 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+export const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  margin-bottom: 8rem;
+  text-align: justify;
+
+  img {
+    width: 100%;
+  }
+
+  h1, h2, h3 {
+    color: ${({ theme }) => theme.colors["blue"]};
+  }
+
+  ul {
+    list-style: inherit;
+    padding-left: 1.5rem;
+  }
+
+  pre {
+    background: ${({ theme }) => theme.colors["base-post"]};
+    padding: 1rem;
+    line-height: 160% !important;
+
+    code {
+      background: none !important;
+      padding: 0 !important;
+      margin: 0 !important;
+      width: 100%;
+      max-width: 1024px;
+      overflow-x: scroll;
+      white-space: pre-wrap;
+      word-wrap: break-word;
+    }
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors["blue"]};
+  }
+`
+
+export const PostHeaderContainer = styled.div`
+  padding: 2rem 2.5rem;
+  background-color: ${({ theme }) => theme.colors['base-profile']};
+  border-radius: 10px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  strong {
+    font-size: 1.5rem;
+    font-weight: bold;
+    line-height: 130%;
+    color: ${({ theme }) => theme.colors["base-title"]};
+  }
+`
+
+export const PostHeaderActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Action = styled(Link)`
+  display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    text-decoration: none;
+
+    font-size: 0.75rem;
+    line-height: 160%;
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors['blue']};
+
+    border-bottom: 1px solid transparent;
+
+    &:hover {
+      border-bottom: 1px solid ${({ theme }) => theme.colors['blue']};
+    }
+`
+
+export const PostInfoContainer = styled.div`
+  display: flex;
+  gap: 1.5rem;
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    line-height: 160%;
+    color: ${({ theme }) => theme.colors['base-label']}
+  }
+`
